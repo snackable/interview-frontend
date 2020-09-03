45a9c9af1ef826ed55e1e62cf6e167a4c75b735c
Lembit Lõpp <lembit.lopp@gmail.com>
2020-09-03 19:32:06 +0300

    One more cleanup oops

--- pages/files/[id].js
+++ pages/files/[id].js
@@ -3,15 +3,11 @@ import FilesPage from "../../components/FilesPage";
 import FileDetail from "../../components/FileDetail";
 import { loadFile } from '../../api';
 
-const FileDetailPage = ({ file }) => {
-  console.log(file);
-
-  return (
-    <FilesPage>
-      <FileDetail file={file} />
-    </FilesPage>
-  );
-};
+const FileDetailPage = ({ file }) => (
+  <FilesPage>
+    <FileDetail file={file} />
+  </FilesPage>
+);
 
 FileDetailPage.getInitialProps = async ({ req, query }) => {
   const file = await loadFile(query.id);
